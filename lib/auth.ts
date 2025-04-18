af8f8fbacc14cd98969b47d23fdfd17f957928b8
Prakash Dey <91982512+prakash-dey@users.noreply.github.com>
2025-04-19 02:14:03 +0530

    added interview route

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken";
 import User from "@/models/User.model";
 import connectDB from "@/Database/config";
 import { cookies } from "next/headers";
+import Interview from "@/models/Interview.model";
 await connectDB();
  
 export const isUserAuthenticated = async () => {
@@ -27,5 +28,32 @@ export const isUserAuthenticated = async () => {
     }
 }
 
+export const getInterviewById = async (id: string) => {
+    try {
+        const interview = await Interview.findById(id);
+        if (!interview) {
+            return null;
+        }
+        return interview;
+    } catch (error: any) {
+        console.error("Error fetching interview:", error);
+        return null;
+    }
+}
 
-
+export const getFeedbackByInterviewId = async (params:{interviewId: string, userId:string}) => {
+    try {
+        const { interviewId, userId } = params;
+        const feedback = await Interview.findOne({
+            _id: interviewId,
+            userId: userId,
+        });
+        if (!feedback) {
+            return null;
+        }
+        return feedback;
+    } catch (error: any) {
+        console.error("Error fetching feedback:", error);
+        return null;
+    }
+}
